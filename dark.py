b1571909ee1b93005b9b12e9194d6da3778f640f
rezadkim <43247492+rezadkim@users.noreply.github.com>
2019-05-18 12:17:47 +0700

    dark.py

--- dark.py
+++ dark.py
@@ -1,3 +1,2 @@
-print "\033[1;91mTools ini Menggunakan Kunci Lisensi"
-print "Jika belum punya Anda bisa Membelinya melalui :"
-print "\033[1;97mWA : 0895611252563"
+print "\033[1;91mTools ditutup sementara"
+print "Akan ada yang Free"
