a2ef3b7efd1aa2ebca6e8c876abc434006aa90a6
rezadkim <43247492+rezadkim@users.noreply.github.com>
2019-08-24 11:43:47 +0700

    Create install.py

---
+++ install.py
@@ -0,0 +1,7 @@
+import os,sys
+
+os.system("pkg install python2")
+os.system("pip2 install requests")
+os.system("pip2 install mechanize")
+os.system("pip2 install bs4")
+os.system("cd dark-fb && python2 dark.pyc")
