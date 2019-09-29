7d707c6d75c6a29eec385d87d2bf743f25449bc6
rezadkim <43247492+rezadkim@users.noreply.github.com>
2019-09-29 15:55:06 +0700

    install.py

--- install.py
+++
@@ -1,7 +0,0 @@
-import os,sys
-
-os.system("pkg install python2")
-os.system("pip2 install requests")
-os.system("pip2 install mechanize")
-os.system("pip2 install bs4")
-os.system("python2 dark.py")
